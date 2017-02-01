7c98c4098247a6941f2ec51d09eab25aa58f99f0
smartm13 <smartm13@gmail.com>
2017-02-01 20:34:51 +0530

    Made better home page
    main page little better ..uses ajax now

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ app.config['DEBUG'] = True
 #a=['wsfd','fdsf','fdffafaaa']
 @app.route('/lai_lidho')
 def res():
-        app.logger.warning('A warning occurred (%d apples)', 42)
-        app.logger.error('An error occurred')
-        app.logger.info('Info')
-	a = maps.find_sun_pos(request.args['par1'],request.args['par2'])
-        try:
-                return render_template('result.html', res_list = a)
-        except:return "Locha"
-	
+    import maps
+    a = maps.find_sun_pos(request.args['par1'],request.args['par2'])
+    try:
+    	return render_template('result.html', res_list = a)
+
+    except:return "Locha"
+
 @app.route('/')
-def hello():
+@app.route('/sun')
+def sunhello():
     """Return a friendly HTTP greeting."""
     return """
 	<html>
@@ -32,7 +32,7 @@ def hello():
 	<input id="str1" name="par1" type="text"/>
 	<hr>
 	<label for="str2">destination</label>
-	<input id="str2" name="par2" type="text"/>
+	<input id="str2" name="par2" type="text" onkeydown="if (event.keyCode == 13) document.getElementById('clickme').click()"/>
 	<hr>
 	<button id="clickme">Calculate</button>
 	<a href="/help">help</a>
@@ -43,6 +43,7 @@ def hello():
 	document.getElementById("clickme").addEventListener("click", clicked);
 	function clicked()
 	{
+    document.getElementById("output").innerHTML = "Please Wait.";
 	var a=document.getElementById("str1").value;
 	var b=document.getElementById("str2").value;
 	var xhttp = new XMLHttpRequest();
@@ -50,13 +51,17 @@ def hello():
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       document.getElementById("output").innerHTML = xhttp.responseText;
     }
+    if (xhttp.readyState == 4 && xhttp.status != 200) {
+          document.getElementById("output").innerHTML = "Failed";
+
+    }
   };
   xhttp.open("GET", "lai_lidho?par1=".concat(a).concat("&par2=").concat(b), true);
   xhttp.send();
 }
 
 	</script>
-	
+
 	<hr><hr>
 	<p id="output">	</p>
 	</body>
